e341589742daae318c195a50102b1ca93941999e
Fernando Colom <fernando_colom@janis.im>
2025-05-09 18:43:19 -0300

    delete process and add flag debug

--- tests/instance.test.js
+++ tests/instance.test.js
@@ -1,6 +1,7 @@
 import DateHandler from '../lib/index.js';
 import locales from '../lib/locales.js';
 import { strict as assert } from 'assert';
+import { spy } from 'sinon';
 
 describe('Instance DateHandler', () => {
 	it('expect locale en when args are empty ', () => {
@@ -16,5 +17,18 @@ describe('Instance DateHandler', () => {
 	it('expect locale es when is define ', () => {
 		const Dates = new DateHandler({ locale: 'es' });
 		assert.equal(Dates.locale, locales.es);
+	});
+
+	it('should call logError (console.error) when debug is true and an error occurs', () => {
+		const errorSpy = spy(console, 'error');
+		const Dates = new DateHandler({ debug: true });
+		Dates.format('invalid-date', '');
+		Dates.formatDelivery('invalid-date', '');
+
+		assert.equal(errorSpy.called, true);
+		assert(errorSpy.calledTwice);
+		assert(errorSpy.calledWith('Argument must be a valid Date'));
+
+		errorSpy.restore();
 	});
 });
